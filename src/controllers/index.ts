65b157124fa86381a7202b8a47cb756829e41f7e
Marcelo Block Teixeira <marcelochb@gmail.com>
2023-03-02 22:31:10 -0300

    feat: IControllerFormData

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,9 +1,14 @@
-interface IGetController<T> {
+
+export interface IControllerGetData<T> {
   data: T;
   loading: boolean;
   error: boolean;
 }
 
-export interface IController<T> {
-  getController:IGetController<T>
+export type IControllerFormData<T> = IControllerGetData<T> & {
+  onSubmit: (e?: React.FormEvent<HTMLFormElement> | undefined) => void,
+  onChange: {
+    (e: React.ChangeEvent<any>): void;
+    <T_1 = string | React.ChangeEvent<any>>(field: T_1): T_1 extends React.ChangeEvent<any> ? void : (e: string | React.ChangeEvent<any>) => void;
+},
 }
